b24d4226972b564d95a7375e43229e9e9894546b
paphonb <paphonb@gmail.com>
2019-10-17 18:35:05 +0700

    Launcher3: Add simple iconpack support
    based on
    https://gerrit.dirtyunicorns.com/c/4780/
    
    paphonb's edits:
    - Override IconProvider instead of modifying the cache mechanism
    - Listen to pref changes in ConfigMonitor
    
    Change-Id: I69188ae2a6cddd163b211b29e139fb84cd79e80d

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -108,6 +108,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
     public int numFolderColumns;
     public float iconSize;
     public String iconShapePath;
+    public String iconPack;
     public float landscapeIconSize;
     public int iconBitmapSize;
     public int fillResIconDpi;
@@ -153,6 +154,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
         numFolderColumns = p.numFolderColumns;
         iconSize = p.iconSize;
         iconShapePath = p.iconShapePath;
+        iconPack = p.iconPack;
         landscapeIconSize = p.landscapeIconSize;
         iconBitmapSize = p.iconBitmapSize;
         iconTextSize = p.iconTextSize;
@@ -273,6 +275,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
 
         iconSize = displayOption.iconSize;
         iconShapePath = getIconShapePath(context);
+        iconPack = IconPackProvider.getCurrentIconPack(context);
         landscapeIconSize = displayOption.landscapeIconSize;
         iconBitmapSize = ResourceUtils.pxFromDp(iconSize, displayInfo.metrics);
         iconTextSize = displayOption.iconTextSize;
@@ -372,7 +375,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
         }
 
         if (iconSize != oldProfile.iconSize || iconBitmapSize != oldProfile.iconBitmapSize ||
-                !iconShapePath.equals(oldProfile.iconShapePath)) {
+                !iconShapePath.equals(oldProfile.iconShapePath) || !iconPack.equals(oldProfile.iconPack)) {
             changeFlags |= CHANGE_FLAG_ICON_PARAMS;
         }
         if (!iconShapePath.equals(oldProfile.iconShapePath)) {
