LineageOS/android_packages_apps_Trebuchet

Workspace: Allow resizing any widget
Signed-off-by: Joey Rizzoli <joey@lineageos.org>

src/com/android/launcher3/Workspace.java
@@ -2550,8 +2550,7 @@ public class Workspace extends PagedView
 
                         final LauncherAppWidgetHostView hostView = (LauncherAppWidgetHostView) cell;
                         AppWidgetProviderInfo pInfo = hostView.getAppWidgetInfo();
-                        if (pInfo != null && pInfo.resizeMode != AppWidgetProviderInfo.RESIZE_NONE
-                                && !d.accessibleDrag) {
+                        if (pInfo != null && !d.accessibleDrag) {
                             mDelayedResizeRunnable = new Runnable() {
                                 public void run() {
                                     if (!isPageInTransition()) {

AOSiP/platform_packages_apps_Launcher3

Change-Id: Iba122eb45d4ba81e53267ff341addfe281cf02fe

src/com/android/launcher3/Workspace.java
@@ -1863,8 +1863,7 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
                         final LauncherAppWidgetHostView hostView = (LauncherAppWidgetHostView) cell;
                         AppWidgetProviderInfo pInfo = hostView.getAppWidgetInfo();
-                        if (pInfo != null && pInfo.resizeMode != AppWidgetProviderInfo.RESIZE_NONE
-                                && !options.isAccessibleDrag) {
+                        if (pInfo != null && !options.isAccessibleDrag) {
                             onCompleteRunnable = () -> {
                                 if (!isPageInTransition()) {
                                     AppWidgetResizeFrame.showForWidget(hostView, cellLayout);